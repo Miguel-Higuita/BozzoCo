fe84339fb3ba305cf8fe81ae3715eea711474ae7
Higuita <mhiguitahincapie@gmail.com>
2025-09-26 17:22:10 -0500

    Tercer commit con cambios en la pagina principal y nuevos estilos para la pagina "adentro.php"

---
+++ adentro.php
@@ -0,0 +1,21 @@
+<?php 
+require 'includes/funciones.php';
+incluirTemplates('header')
+?>
+
+    <main class="contenedor">
+        <h2 class="nombre_persona">Nombre completo</h2>
+        <div class="info_persona">
+        <div class="espacio_foto">
+            <img class="foto_persona" src="src/scss/img/nino-fontanero-lindo-dibujos-animados_33070-2238.jpg" alt="foto de perfil">
+        </div>
+        <div class="texto_persona">
+            <p>Descripción trabajo</p>
+            <p>Teléfono</p>
+            <p>Correo</p>
+        </div>
+        </div>
+        
+    </main>
+
+<?php include 'includes/templates/footer.php'; ?>
--- index.php
+++ index.php
@@ -5,15 +5,15 @@ incluirTemplates('header')
 
     <main class="contenedor">
         <section>
-                <a href="admin/propiedades/crear.php" class="boton boton-verde">Nuevo servicio</a>
+             
             <!-- inicio producto -->
             <div class="grid">
                 <div class="servicio1">
-                <a href="#">
-                    <img class="servicio__imagen" src="#" alt="#" />
+                <a href="adentro.php">
+                    <img class="servicio__imagen" src="src/scss/img/nino-fontanero-lindo-dibujos-animados_33070-2238.jpg" alt="" />
                     <div class="servicio__informacion">
-                    <b><p class="servicio__nombre">Lorem</p>
-                    <p class="servicio__labor">Lorem ipsum dolor sit amet </p></b>
+                    <b><p class="servicio__nombre">Plomeria</p>
+                    <p class="servicio__labor">Miguel Angel</p></b>
                     </div>
                 </a>
                 </div>
@@ -23,7 +23,7 @@ incluirTemplates('header')
                 <a href="#">
                     <img class="servicio__imagen" src="#" alt="#" />
                     <div class="servicio__informacion">
-                    <b><p class="servicio__nombre">#</p>
+                    <b><p class="servicio__nombre"></p>
                     <p class="servicio__labor">$$</p></b>
                     </div>
                 </a>
@@ -51,8 +51,44 @@ incluirTemplates('header')
                     </div>
                 </a>
                 </div>
+            <!-- inicio servicio -->
+                <div class="servicio1">
+                <a href="#">
+                    <img class="servicio__imagen" src="#" alt="#" />
+                    <div class="servicio__informacion">
+                    <b><p class="servicio__nombre">#</p>
+                    <p class="servicio__labor">$$</p></b>
+                    </div>
+                </a>
+                </div>
+                <div class="servicio1">
+                <a href="#">
+                    <img class="servicio__imagen" src="#" alt="#" />
+                    <div class="servicio__informacion">
+                    <b><p class="servicio__nombre">#</p>
+                    <p class="servicio__labor">$$</p></b>
+                    </div>
+                </a>
+                </div>
             </div>
             <!-- termina servicio -->
+             <div>
+                <a href="admin/propiedades/crear.php" class="boton boton-verde">Nuevo servicio</a>
+             </div>
         </section>
+        <div class="mensajeria">
+        <a href="https://api.whatsapp.com/send?phone=3006425967 &text=Hola%20,%20Estoy%20Interesado%20por%20un%20trabajo,%20mi%20nombre%20es:%20" target="_blank">
+            <picture class="icon-wsp">
+                <source srcset="src/scss/img/logowhatsApp.avif" type="image/avif">
+                <source srcset="src/scss/img/logowhatsApp.webp" type="image/webp">
+                <img loading="lazy" width="100%" height="100%" src="src/scss/img/logowhatsApp.png" alt="logowhatsApp">
+            </picture>
+        </a>
+        <div class="caja">
+            <a href="https://api.whatsapp.com/send?phone=30111 &text=Hola%20,%20Estoy%20Interesado%20por%20un%20trabajo,%20mi%20nombre%20es:%20" target="_blank">
+            <p>Asesoria gratuita</p></a>
+        </div>
+ 
+    </div>
     </main>
 <?php include 'includes/templates/footer.php'; ?>
