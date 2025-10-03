8c607fe220c0ea6a8e4bbf43301e73b3913265c1
antonioupb7 <antonioupb7@gmail.com>
2025-10-03 15:38:37 -0500

    actualizacion base de datos trabaja desde xampp

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+
+// concexion para trabajar desde UPB
 function conectarDB() {
     $db = mysqli_connect('localhost', 'root', '', 'bozzo', 3305);
 
@@ -12,3 +14,18 @@ function conectarDB() {
 
     return $db;
 }
+
+
+// concexion para trabajar desde otro sitio
+// function conectarDB() {
+//     $db = mysqli_connect('localhost', 'root', '', 'bozzo');
+
+//     if (!$db) {
+//         echo "Error de conexión: ";
+//         exit; // detiene la ejecución si no hay conexión
+//     }
+
+//     // echo "✅ Conexión exitosa a la BD"; // puedes dejarlo solo para pruebas
+
+//     return $db;
+// }
