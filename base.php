96edb4e6c5037c81fd55d50c7d23263124b791e2
upbantonio <gildardo.arenas@upb.edu.co>
2025-10-08 02:02:04 -0500

    pagina anuncios dinamica, faltan stilos

---
+++ base.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'includes/funciones.php';
+
+incluirTemplate('header');
+?>
+
+
+    <body>
+        <h1>###########     Nombre de la pagina    ###########</h1>
+    </body>
+
+    <?php incluirTemplate('footer');?>
--- index.php
+++ index.php
@@ -4,78 +4,15 @@ incluirTemplate('header')
 ?>
 
     <main class="contenedor">
-        <section>
-             
-            <!-- inicio producto -->
-            <div class="grid">
-                <div class="servicio1">
-                <a href="adentro.php">
-                    <img class="servicio__imagen" src="src/scss/img/nino-fontanero-lindo-dibujos-animados_33070-2238.jpg" alt="" />
-                    <div class="servicio__informacion">
-                    <b><p class="servicio__nombre">Plomeria</p>
-                    <p class="servicio__labor">Miguel Angel</p></b>
-                    </div>
-                </a>
-                </div>
-                <!-- termina servicio -->
-            <!-- inicio servicio -->
-                <div class="servicio1">
-                <a href="#">
-                    <img class="servicio__imagen" src="#" alt="#" />
-                    <div class="servicio__informacion">
-                    <b><p class="servicio__nombre"></p>
-                    <p class="servicio__labor">$$</p></b>
-                    </div>
-                </a>
-                </div>
+        
+     <?php 
+      $limite = 6;
+      include 'includes/templates/anuncios.php'
+      ?>
 
-            <!-- termina servicio -->
-            <!-- inicio servicio -->
-                <div class="servicio1">
-                <a href="#">
-                    <img class="servicio__imagen" src="#" alt="#" />
-                    <div class="servicio__informacion">
-                    <b><p class="servicio__nombre">#</p>
-                    <p class="servicio__labor">$$</p></b>
-                    </div>
-                </a>
-                </div>
-            <!-- termina servicio -->
-            <!-- inicio servicio -->
-                <div class="servicio1">
-                <a href="#">
-                    <img class="servicio__imagen" src="#" alt="#" />
-                    <div class="servicio__informacion">
-                    <b><p class="servicio__nombre">#</p>
-                    <p class="servicio__labor">$$</p></b>
-                    </div>
-                </a>
-                </div>
-            <!-- inicio servicio -->
-                <div class="servicio1">
-                <a href="#">
-                    <img class="servicio__imagen" src="#" alt="#" />
-                    <div class="servicio__informacion">
-                    <b><p class="servicio__nombre">#</p>
-                    <p class="servicio__labor">$$</p></b>
-                    </div>
-                </a>
-                </div>
-                <div class="servicio1">
-                <a href="#">
-                    <img class="servicio__imagen" src="#" alt="#" />
-                    <div class="servicio__informacion">
-                    <b><p class="servicio__nombre">#</p>
-                    <p class="servicio__labor">$$</p></b>
-                    </div>
-                </a>
-                </div>
-            </div>
-            <!-- termina servicio -->
-             <div>
-                <a href="admin/propiedades/crear.php" class="boton boton-verde">Nuevo servicio</a>
-             </div>
-        </section>
+
+
+
         <div class="mensajeria">
         <a href="https://api.whatsapp.com/send?phone=3006425967 &text=Hola%20,%20Estoy%20Interesado%20por%20un%20trabajo,%20mi%20nombre%20es:%20" target="_blank">
             <picture class="icon-wsp">
